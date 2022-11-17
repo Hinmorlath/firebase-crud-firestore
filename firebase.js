6d93efab8e82f66abe5e83f70fcf8dcfeb1fefa1
Hinmorlath <josepolomanuel2@gmail.com>
2022-11-17 13:42:25 -0600

    Conectando con FireStore

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDw5aJuiAR1PhdlQQb8ZNf1pghWpGBA9Z4",
+  authDomain: "fir-javascript-crud-9bf3c.firebaseapp.com",
+  projectId: "fir-javascript-crud-9bf3c",
+  storageBucket: "fir-javascript-crud-9bf3c.appspot.com",
+  messagingSenderId: "997217852817",
+  appId: "1:997217852817:web:3de031f519ea3cc1d04e30"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
